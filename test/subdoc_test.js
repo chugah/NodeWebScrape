ab88e1aa619aa76a4367116eab696c803aee32b7
Sami Barghshoon <chugah@users.noreply.github.com>
2017-01-10 11:43:24 -0500

    Added create and delete subdocument testing

--- test/subdoc_test.js
+++ test/subdoc_test.js
@@ -12,7 +12,45 @@ describe('subdocument', () => {
 		jean.save()
 			.then(() => User.findOne({ first_name: 'Jean' }))
 			.then((user) => {
-				assert(user.posts[0].title === 'Eating Sush');
+				assert(user.posts[0].title === 'Eating Sushi');
+			});
+		done();
+	});
+
+	it('should add a new post (subdoc) to an existing user', (done) => {
+		const dave = new User({
+			first_name: 'Dave',
+			posts: []
+		});
+		dave.save()
+			.then(() => User.findOne({ first_name: 'Dave' }))
+			.then((user) => {
+				user.posts.push({ title: 'Cooking Italian' });
+				return user.save();
+			})
+			.then(() => User.findONe({ name: 'Dave'}))
+			.then((user) => {
+				assert(user.posts[0].title === 'Cooking Italian');
+			});
+		done();
+	});
+
+	it('should delete a post (subdoc) to an existing user', (done) => {
+		const francois = new User({
+			first_name: 'Francois',
+			posts: [{
+				title: 'The Art of Making Bread'
+			}]
+		});
+		francois.save()
+			.then(() => User.findOne({ first_name: 'Francois' }))
+			.then((user) => {
+				user.posts[0].remove({ title: 'The Art of Making Bread' });
+				return user.save();
+			})
+			.then(() => User.findOne({ name: 'Francois' }))
+			.then((user) => {
+				assert(user.posts.length === 0);
 			});
 		done();
 	});
